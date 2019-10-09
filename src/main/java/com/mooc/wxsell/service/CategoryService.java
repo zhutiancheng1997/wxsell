04f0f16db0b00d2f2482f7673e2fe1a4c0d0169c
1524495584@qq.com <1524495584@qq.com>
2019-10-09 17:44:44 +0800

    4-3

--- src/main/java/com/mooc/wxsell/service/CategoryService.java
+++ src/main/java/com/mooc/wxsell/service/CategoryService.java
@@ -2,6 +2,12 @@ package com.mooc.wxsell.service;
 
 import com.mooc.wxsell.dataobject.ProductCategory;
 
+import java.util.List;
+
 public interface CategoryService {
-    ProductCategory findOne(Integer)
+    ProductCategory findOne(Integer categoryId);
+    List<ProductCategory> findAll();
+    List<ProductCategory> findByCategoryTypeIn(List<Integer> categoryTypeList);
+    ProductCategory save(ProductCategory pc);
+//    hh
 }
